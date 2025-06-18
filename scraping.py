16936ffc770398c9b155d6686638481bfb4c196a
DavideMasiDev <davidemasi@live.com>
2025-06-18 20:58:02 +0200

    aggiornato file scraping

--- scraping.py
+++ scraping.py
@@ -1,25 +1,15 @@
 import requests
 import csv
+import time
 from datetime import datetime, timezone
 
 API_KEY = "cae48f2d7db01e9403cce10a083597c6e3b49696"
-APPID = input("Inserisci l'AppID del gioco Steam: ").strip()
 COUNTRY = "IT"
 REGION = "eu"
 SHOP = 61
+TIMEOUT = 1
 
-# 1. Ottieni nome gioco da Steam
-def get_game_name(appid):
-    url = f"https://store.steampowered.com/api/appdetails"
-    params = {"appids": appid, "cc": COUNTRY}
-    r = requests.get(url, params=params)
-    data = r.json()
-    try:
-        return data[appid]["data"]["name"]
-    except:
-        return None
-
-# 2. Ottieni plain da IsThereAnyDeal via search
+# 1. Ottieni plain da IsThereAnyDeal via search
 def get_plain_from_name(name):
     url = "https://api.isthereanydeal.com/games/search/v1"
     params = {
@@ -28,9 +18,12 @@ def get_plain_from_name(name):
     }
     r = requests.get(url, params=params)
     results = r.json()
-    return results[0]["id"]
+    if results and results[0] and results[0]["id"]:
+        return results[0]["id"]
+    else:
+        return None
 
-# 3. Ottieni storico prezzi
+# 2. Ottieni storico prezzi
 def get_price_history(plain, release_date):
     url = "https://api.isthereanydeal.com/games/history/v2"
     params = {
@@ -44,7 +37,7 @@ def get_price_history(plain, release_date):
     data = r.json()
     return data
 
-# 4. Salva in CSV
+# 3. Salva in CSV
 def save_to_csv(price_data, filename):
     with open(filename, mode="w", newline="", encoding="utf-8") as f:
         writer = csv.writer(f)
@@ -65,7 +58,7 @@ def save_to_csv(price_data, filename):
             ])
     print(f"[✓] Dati salvati in {filename}")
 
-# 5. Ottieni data rilascio
+# 4. Ottieni data rilascio
 def get_release_date(plain):
     url = "https://api.isthereanydeal.com/games/info/v2"
     params = {
@@ -77,24 +70,24 @@ def get_release_date(plain):
     return data["releaseDate"]
 
 # === ESECUZIONE ===
-name = get_game_name(APPID)
-if not name:
-    print("[!] Nome del gioco non trovato tramite Steam.")
-else:
-    print(f"[i] Trovato nome: {name}")
+
+with open('giochi.txt', 'r', encoding='utf-8') as file:
+    names = [riga.strip() for riga in file]
+
+for name in names:
+    print(f"[i] Cerco 'plain' per: {name}")
     plain = get_plain_from_name(name)
     if not plain:
         print("[!] Nessun 'plain' trovato per questo gioco.")
     else:
         print(f"[i] Plain corrispondente: {plain}")
         release_date = get_release_date(plain)
-        release_date = datetime.strptime(release_date, "%Y-%m-%d").replace(tzinfo=timezone.utc).isoformat()
         if not release_date:
             print("[!] Nessuna data di rilascio trovata")
-
         else:
+            release_date = datetime.strptime(release_date, "%Y-%m-%d").replace(tzinfo=timezone.utc).isoformat()
             print(f"[i] Data di rilascio: {release_date}")
             data = get_price_history(plain, release_date)
-            print(f"Data: {data}")
-            save_to_csv(data, f"storico_prezzi_{name}.csv")
+            save_to_csv(data, f"./storici/storico_prezzi_{name}.csv")
+    time.sleep(TIMEOUT)
 
