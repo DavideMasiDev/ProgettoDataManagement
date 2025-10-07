add31eb3d86f67828a21dc978a2f967994be845b
DavideMasiDev <davidemasi@live.com>
2025-10-08 00:23:43 +0200

    fix: implementato main su script date DWH

--- ETL/DWH/dim01_date_DWH.py
+++ ETL/DWH/dim01_date_DWH.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine, text
 import pandas as pd
 
 
-DB_URI = "postgresql+psycopg2://postgres:postgres@localhost:5432/steam_dwh"
+DB_URI = "postgresql+psycopg2://postgres:postgres@localhost:5432/steamdb"
 
 TABLE_NAME = "date"
 SCHEMA_NAME = "DWH"
@@ -16,7 +16,7 @@ def load_records(start_date: datetime, end_date: datetime):
     current_date = start_date
     while current_date <= end_date:
         records.append({
-            "date": current_date.date(),
+            "full_date": current_date.date(),
             "year": current_date.year,
             "month": current_date.month,
             "day": current_date.day,
@@ -28,18 +28,14 @@ def load_records(start_date: datetime, end_date: datetime):
 
     # Chiamata al db per caricare le date generate
 
-    # 1) Load and normalize
-    df = pd.DataFrame(records, columns=["date", "year", "month", "day", "day_of_week", "day_name", "month_name"])
+    # Load and normalize
+    df = pd.DataFrame(records, columns=["full_date", "year", "month", "day", "day_of_week", "day_name", "month_name"])
     print(f"* Records parsed: {len(df)}")
 
-    # 2) Save CSV (staging)
-    df.to_csv(OUTPUT_FILE, index=False, encoding="utf-8")
-    print(f"* CSV creato: {OUTPUT_FILE}")
-
-    # 3) Connect to Postgres
+    # Connect to Postgres
     engine = create_engine(DB_URI)
 
-    # 4) Carico dati in Postgres
+    # Carico dati in Postgres
     df.to_sql(TABLE_NAME, engine, schema=SCHEMA_NAME, if_exists="append", index=False, method="multi", chunksize=5000)
     print(f"* Inseriti {len(df)} record nella tabella '{TABLE_NAME}'")
 
@@ -50,3 +46,6 @@ def main():
     start_date = datetime(2000, 1, 1)
     end_date = datetime(2030, 12, 31)  
     load_records(start_date, end_date)
+
+if __name__ == "__main__":
+    main()
