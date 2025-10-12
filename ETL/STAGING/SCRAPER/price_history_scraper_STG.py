f3554d7c227b681d7c8624d57e046ad59e2352e3
DavideMasiDev <davidemasi@live.com>
2025-10-12 18:59:45 +0200

    fix: adeguato metodo di loading per price_history

--- ETL/STAGING/SCRAPER/price_history_scraper_STG.py
+++ ETL/STAGING/SCRAPER/price_history_scraper_STG.py
@@ -1,14 +1,13 @@
 import requests
-import csv
 import time
 import sys
 import pandas as pd
 from sqlalchemy import create_engine
 from datetime import datetime, timezone
+from utils.db_utils import connect_db, DB_URI
 
 # TODO: add your key here
-API_KEY = "cae48f2d7db01e9403cce10a083597c6e3b49696"
-DB_URI = "postgresql+psycopg2://postgres:postgres@localhost:5432/steamdb"
+API_KEY = "your_api_key"
 SCHEMA_NAME = "STAGING"
 TABLE_NAME = "price_history"
 COUNTRY = "IT"
@@ -139,36 +138,30 @@ def barra_di_caricamento(iterazione, totale, start_time, lunghezza=30):
         print()
 
 # === ESECUZIONE ===
-def load_price_history_scraper(db_uri, schema_name, table_name, api_key, games=None):
+def load_records(games=None):
     if games is None:
         games = []
 
-    conn = init_db_connection(db_uri)
+    conn = connect_db(DB_URI).connect()
     index = 0
     totale_giochi = len(games)
     start_time = time.time()
     for game in games:
 
         if DEBUG: print(f"[i] Cerco 'plain' per: {game["name"]}")
-        plain = get_plain_from_name(game["name"], api_key)
+        plain = get_plain_from_name(game["name"], API_KEY)
         if not plain:
             if DEBUG: print("[!] Nessun 'plain' trovato per questo gioco.")
         else:
             if DEBUG: print(f"[i] Plain corrispondente: {plain}")
-            release_date = get_release_date(plain, api_key)
+            release_date = get_release_date(plain, API_KEY)
             if not release_date:
                 if DEBUG: print("[!] Nessuna data di rilascio trovata")
             else:
                 release_date = datetime.strptime(release_date, "%Y-%m-%d").replace(tzinfo=timezone.utc).isoformat()
                 if DEBUG: print(f"[i] Data di rilascio: {release_date}")
-                data = get_price_history(plain, release_date, api_key)
-                save_to_db(data, game["steam_appid"], game["name"], conn, schema_name, table_name)
+                data = get_price_history(plain, release_date, API_KEY)
+                save_to_db(data, game["steam_appid"], game["name"], conn, SCHEMA_NAME, TABLE_NAME)
         barra_di_caricamento(index, totale_giochi, start_time)
         index += 1
         time.sleep(TIMEOUT)
-
-def main():
-    load_price_history_scraper(DB_URI, SCHEMA_NAME, TABLE_NAME, API_KEY)
-
-if __name__ == "__main__":
-    main()
--- ETL/load_staging.py
+++ ETL/load_staging.py
@@ -1,6 +1,7 @@
 from STAGING.game_player_region_STG import load_records
 from STAGING.raw_data_STG import load_records
 from STAGING.released_games_STG import load_records
+from STAGING.SCRAPER.price_history_scraper_STG import load_records as load_records_price_history
 
 
 def load_staging():
