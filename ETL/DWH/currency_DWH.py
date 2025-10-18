79f1442fe08222baec11c0a285df295e743890a6
Emanuele Patriarca <patriarcaemanuele@gmail.com>
2025-10-18 18:09:43 +0200

    currency_DWH

--- ETL/DWH/currency_DWH.py
+++ ETL/DWH/currency_DWH.py
@@ -2,11 +2,14 @@ from utils.db_utils import select_rows, insert_rows
 
 def load_records():
 
-    query = 'select distinct currency from "STAGING".price_history'
-
+    query = 'select distinct currency as currency_code from "STAGING".price_history'
     currencies = select_rows(query)
 
-    print(currencies)
+    currency_name = {"EUR": "Euro", "USD": "Dollaro Statunitense", "GBP": "Sterlina Britannica"}
+
+    currencies["currency_name"] = currencies["currency_code"].map(currency_name)
+
+    insert_rows("DWH", "currency", currencies)
 
     return None
 
