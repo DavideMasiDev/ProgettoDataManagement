6efb868a92e0455d6274c971c4b1fb493f243aa6
DavideMasiDev <davidemasi@live.com>
2025-10-18 18:45:11 +0200

    feat: aggiunto controllo presenza nuovi records in steam_game prima di inserire su DWH

--- ETL/DWH/steam_game_DWH.py
+++ ETL/DWH/steam_game_DWH.py
@@ -1,15 +1,24 @@
-from utils.db_utils import select_rows, insert_rows
+from utils.db_utils import select_rows, insert_rows, find_new_records
+
 
 def load_records():
 
-    query = ('select rd.steam_appid, rd.name as game_name, rd."type", rd.fullgame, dt.date_pk as release_date_pk '
+    query_staging = ('select rd.steam_appid, rd.name as game_name, rd."type", rd.fullgame, dt.date_pk as release_date_pk '
              'from "STAGING".raw_data rd '
              'join "STAGING".released_game rg on rd.steam_appid = rg.steam_appid '
              'join "DWH".date dt on dt.full_date = rg.release_date')
 
-    steam_game_rows = select_rows(query)
+    query_dwh = ('select sg.steam_appid, sg.game_name, sg."type", sg.fullgame, sg.release_date_pk '
+                 'from "DWH".steam_game sg')
 
-    insert_rows("DWH", "steam_game", steam_game_rows)
+    staging_rows = select_rows(query_staging)
+    dwh_rows = select_rows(query_dwh)
+
+    new_rows = find_new_records(staging_rows, dwh_rows, key_column='steam_appid')
+
+    print(new_rows)
+
+    insert_rows("DWH", "steam_game", new_rows)
 
     return None
 
