1e4b080a46f8cb54e2a833de0b7a0e1e972d5e39
DavideMasiDev <davidemasi@live.com>
2025-11-09 19:49:17 +0100

    feat: aggiunta tabella dei fatti per game_statistics in datamart

--- ETL/DATAMART/game_statistics_fact_DM.py
+++ ETL/DATAMART/game_statistics_fact_DM.py
@@ -1,6 +1,18 @@
+from utils.db_utils import select_rows, insert_rows
 
-def load_records():
 
-    #TODO
+def load_records(data_inizio_periodo, data_fine_periodo):
+    if data_inizio_periodo is not None and data_fine_periodo is not None:
+        query = ("select gsf.*"
+                " from \"DWH\".game_statistics_fact gsf"
+               f" where gsf.dat_fin_val::date >= \'{data_inizio_periodo}\'"
+               f" and gsf.dat_ini_val::date <= \'{data_fine_periodo}\'")
+
+        selected_rows = select_rows(query)
+
+        insert_rows('DATAMART', 'game_statistics_fact', selected_rows)
+
+    else:
+        print('\n[ERROR] data_inizio_periodo and data_fine_periodo can not be None')
 
     return None
