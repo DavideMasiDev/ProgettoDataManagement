79fe9030e54b82af1db2564a4d0319437fe89f09
Emanuele Patriarca <patriarcaemanuele@gmail.com>
2026-01-29 23:50:33 +0100

    Fix currency DWH

--- ETL/DWH/currency_DWH.py
+++ ETL/DWH/currency_DWH.py
@@ -2,7 +2,7 @@ from utils.db_utils import select_rows, insert_rows, find_new_records
 
 def load_records():
 
-    query = 'select distinct currency as currency_code from "STAGING".price_history'
+    query = 'select distinct currency as currency_code from "STAGING".price_history where currency is not null'
     currencies = select_rows(query)
 
     # Recuperiamo i record già inseriti in DWH in modo da non creare duplicati nella tabella,
@@ -14,7 +14,7 @@ def load_records():
 
     currencies = find_new_records(currencies, dwh_currencies, ['currency_code'])
 
-    currency_name = {"EUR": "Euro", "USD": "Dollaro Statunitense", "GBP": "Sterlina Britannica"}
+    currency_name = {"EUR": "Euro", "USD": "Dollaro Statunitense", "GBP": "Sterlina Britannica", "CHF": "Franco Svizzero"}
     currencies["currency_name"] = currencies["currency_code"].map(currency_name)
 
     insert_rows("DWH", "currency", currencies)
---
+++ ETL/utils/prova.py
@@ -0,0 +1,54 @@
+import from STAGING.SCRAPER.price_history_scraper_STG import load_records
+
+games = ()
+
+'Deep Rock Galactic
+The Binding of Isaac: Rebirth
+Papers, Please
+The Walking Dead
+Split Fiction
+Euro Truck Simulator 2
+SANABI
+BeamNG.drive
+Counter-Strike
+Celeste
+Terraria
+Left 4 Dead 2
+South Park™: The Stick of Truth™
+Phoenix Wright: Ace Attorney Trilogy
+Dyson Sphere Program
+WEBFISHING
+Dishonored
+Half-Life 2
+ULTRAKILL
+Plants vs. Zombies GOTY Edition
+STAR WARS™ Empire at War - Gold Pack
+Bongo Cat
+Helltaker
+Slay the Spire
+Mount & Blade: Warband
+Resident Evil 4
+Firework
+A Hat in Time
+Totally Accurate Battle Simulator
+Volcano Princess
+Dispatch
+RimWorld
+Balatro
+MiSide
+Schedule I
+Slime Rancher
+Katana ZERO
+Wallpaper Engine
+Hades
+Half-Life:
+VPet-Simulator
+OneShot
+Pizza Tower
+Vampire Survivors
+Stardew Valley
+Portal
+People Playground
+DELTARUNE
+The Henry Stickmin Collection
+Portal 2
