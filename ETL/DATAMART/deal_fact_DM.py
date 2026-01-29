2b90bb5c9484fd24634607ff1cde7c1176031cbf
Emanuele Patriarca <patriarcaemanuele@gmail.com>
2026-01-30 00:30:27 +0100

    where commented on deal DATAMART

--- ETL/DATAMART/deal_fact_DM.py
+++ ETL/DATAMART/deal_fact_DM.py
@@ -6,8 +6,8 @@ def load_records(data_inizio_periodo, data_fine_periodo):
         query = ("select df.*"
                 " from \"DWH\".deal_fact df"
                 " join \"DWH\".date dt on dt.date_pk = df.deal_date_pk"
-               f" where dt.full_date::date >= \'{data_inizio_periodo}\'"
-               f" and dt.full_date::date <= \'{data_fine_periodo}\'")
+               f" --where dt.full_date::date >= \'{data_inizio_periodo}\'"
+               f" --and dt.full_date::date <= \'{data_fine_periodo}\'")
 
         selected_rows = select_rows(query)
 
